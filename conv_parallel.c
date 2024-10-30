4bc7e7ba652763584785726f627beee2d8230002
Supakit <79649121+Tanwa2545@users.noreply.github.com>
2024-10-30 23:38:41 +0700

    Update conv_parallel.c

--- conv_parallel.c
+++ conv_parallel.c
@@ -23,8 +23,8 @@ int main(){
     #pragma omp parallel for num_threads(8)
     for(int i = 0; i < NA-NF+1; i++){
         int conv_sum = 0;
-        for(int j = NF-1; j >= 0; j--){
-            conv_sum += A[i+(NF-1)-j] * F[j];
+        for(int j = 0; j < NF; j++){
+            conv_sum += A[i+j] * F[NF-1-j];
         }
         R[i] = conv_sum;
     }
