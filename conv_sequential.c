4242088abb2c2ceb1f877b04ecd2ca1e37b610fb
Supakit <79649121+Tanwa2545@users.noreply.github.com>
2024-10-30 23:07:20 +0700

    Update conv_sequential.c

--- conv_sequential.c
+++ conv_sequential.c
@@ -17,22 +17,57 @@ int main(){
     // ---- end input and malloc----
 
     // implement here
-    int *R = malloc(sizeof(int) * (NA - NF + 1));
-
-    for(int i = 0; i < NA; i++){
-        int sum = 0;
-        for(int j = 0; j < NF; j++){
-            sum += A[i+j]*F[NF - j - 1];
+    int *R = malloc(sizeof(int) * (NA-NF+1));
+    for(int i = 0; i < NA-NF+1; i++){
+        int conv_sum = 0;
+        for(int j = NF-1; j >= 0; j--){
+            conv_sum += A[i+(NF-1)-j] * F[j];
         }
-        R[i] = sum;
+        R[i] = conv_sum;
     }
 
     for(int i = 0; i < NA-NF+1; i++){
         printf("%d\n", R[i]);
     }
+    // ---- free memory ----
+    #include<stdio.h>
+#include<stdlib.h>
 
+int main(){
+    // ---- input and malloc A, F ----
+    int NA, NF;
+    scanf("%d %d", &NA, &NF);
+    int *A = malloc(sizeof(int) * NA);
+    int *F = malloc(sizeof(int) * NF);
+
+    for(int i = 0; i < NA; i++){
+        scanf("%d", &A[i]);
+    }
+    for(int i = 0; i < NF; i++){
+        scanf("%d", &F[i]);
+    }
+    // ---- end input and malloc----
+
+    // implement here
+    int *R = malloc(sizeof(int) * (NA-NF+1));
+    for(int i = 0; i < NA-NF+1; i++){
+        int conv_sum = 0;
+        for(int j = NF-1; j >= 0; j--){
+            conv_sum += A[i+(NF-1)-j] * F[j];
+        }
+        R[i] = conv_sum;
+    }
+
+    for(int i = 0; i < NA-NF+1; i++){
+        printf("%d\n", R[i]);
+    }
     // ---- free memory ----
     free(R);
+    free(F);
+    free(A);
+    // ---- end free ----
+    return 0;
+}
     free(F);
     free(A);
     // ---- end free ----
